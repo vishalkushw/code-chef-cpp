8432c11971651bdfe740b17f858e52408d468578
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-09-08 17:35:24 +0530

    Temperature change clisius to kelivin

---
+++ Temperature change clisius to kelivin.cpp
@@ -0,0 +1,12 @@
+//Declare a variable "temperature" and initialise it with a value of 25.5 
+//(in Celsius) and Print it in Celsius and Kelvin(add 273 to temperature 
+//in Celsius). [Note: print the output exactly in the same format as given
+// below.
+
+#include <iostream>
+using namespace std;
+int main(){
+	double temperature=25.5;
+	cout<<"Celsius - "<<temperature<<endl;
+	cout<<"Kelvin - "<<temperature+273;
+}
