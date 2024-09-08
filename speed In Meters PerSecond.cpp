989ecdcbdc33b7ccff228b75617dcf60c8a4a9c0
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-09-08 17:34:49 +0530

    speed In Meters PerSecond

---
+++ speed In Meters PerSecond.cpp
@@ -0,0 +1,11 @@
+//Create a variable named speed1 and assign it the value 36, representing 
+//speed in kilometers per hour. Then, convert and display this speed in
+//meters per second.
+//[Note: 1 km/h = 5/18 m/s]
+
+#include <iostream>
+using namespace std;
+int main(){
+	double speed1=36;
+	cout<<"speed In Meters PerSecond : "<<speed1 * (5.0/18.0);
+}
