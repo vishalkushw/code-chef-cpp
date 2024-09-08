e702cf6043f1cbbb135639e9b1cdd6a93a4682d5
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-09-08 17:34:18 +0530

    Calculate minit end second in houre

---
+++ Calculate minit end second in houre.cpp
@@ -0,0 +1,12 @@
+//Declare a variable hour and initialize it with the value 5 Then, 
+//calculate and print the total number of minutes and seconds present 
+//in this hour.
+
+#include <iostream>
+using namespace std;
+int main(){
+	double hour=5;
+	cout<<"minit : "<<hour*60<<endl;
+	cout<<"Second :"<<hour*60*60;
+	
+}
